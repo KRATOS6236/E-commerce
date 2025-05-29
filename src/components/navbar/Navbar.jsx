893b80e1222253597c3330d9b10622c23651127b
KRATOS6236 <shahilkhangamer@gmail.com>
2025-05-29 13:36:56 +0530

    changes

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -16,10 +16,10 @@ function Navbar() {
         <h1>SHOP.COM</h1>
       </div>
       <div className="nav-pages">
-        <Link to={Home}>Shop</Link>
-        <Link to={Cart}>OnSale</Link>
-        <Link to={New}>New Arrivals</Link>
-        <Link>Brands</Link>
+        <Link to="/">Shop</Link>
+        <Link to="/login">Login</Link>
+        <Link to="/newArrival">New Arrivals</Link>
+        <Link to="/cart">Cart</Link>
       </div>
       <div className="search"></div>
       <div className='nav-btn'></div>
